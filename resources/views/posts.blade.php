346670f347fb31c2204d95e12793633e5a60dfea
Lucas Azambuja Santos <lucasazsantos@gmail.com>
2021-06-09 19:06:04 -0300

    first posts

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app.css">
+    <title>My Blog</title>
+</head>
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post"> My First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam ducimus unde quisquam dolores debitis in saepe? Suscipit deleniti, tempora amet accusantium possimus voluptatum, corporis pariatur dolorum repudiandae fugit, ipsa distinctio.</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post">My second Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam ducimus unde quisquam dolores debitis in saepe? Suscipit deleniti, tempora amet accusantium possimus voluptatum, corporis pariatur dolorum repudiandae fugit, ipsa distinctio.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi laborum eos saepe, ex consequuntur tempore dolor sequi atque molestias quam architecto ea iste, impedit, delectus iusto exercitationem! Quisquam, quasi sapiente!</p>
+    </article>
+
+</body>
+</html>
