cef1d433ec2f707a302054a6782a113ac3744835
Lucas Azambuja Santos <lucasazsantos@gmail.com>
2021-06-14 20:05:07 -0300

    using blade components

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,32 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/app.css">
-    <title>My Blog</title>
-</head>
-
-<body>
+<x-layout>
 
     @foreach ($posts as $post)
         <article>
             <h1>
-                <a href="/posts/ {{$post->slug}}">
-                    {{ $post->title}}
-
+                <a href="/posts/{{ $post->slug }}">
+                    {{ $post->title }}
                 </a>
             </h1>
-            <!-- <p><?= $post->date ?></p> -->
+
             <div>
-                 {{$post->excerpt}}
+                {{ $post->excerpt }}
 
             </div>
 
         </article>
-     @endforeach
-</body>
+    @endforeach
 
-</html>
+</x-layout>
