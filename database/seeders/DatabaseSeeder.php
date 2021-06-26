b64041a924761c064440cddbdf51b08fb09d5d33
Lucas de Azambuja <lucasazsantos@gmail.com>
2021-06-25 23:04:11 -0300

    using factories to make fake data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,58 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            "name" => "Personal",
-            "slug" => "personal",
+        // User::truncate();
+        // Category::truncate();
+        // Post::truncate();
+        $user = User::factory()->create([
+            "name" => "John Doe",
         ]);
 
-        $family = Category::create([
-            "name" => "Family",
-            "slug" => "family",
-        ]);
-
-        $work = Category::create([
-            "name" => "Work",
-            "slug" => "work",
-        ]);
-
-        Post::create([
+        Post::factory(5)->create([
             "user_id" => $user->id,
-            "category_id" => $family->id,
-            "title" => "My family Post",
-            "slug" => "my-first-post",
-            "excerpt" =>
-                "<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit accusamus explicabo adipisci pariatur provident! In atque magni consectetur, perferendis consequatur et id aliquam, sit, quos delectus libero minus repellendus alias! </p>",
-            "body" => "<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium dicta quo atque, ipsum soluta pariatur culpa repellat nostrum quaerat iusto nisi similique odio numquam maiores tempora quas officia sit provident.
-            Necessitatibus error cumque vel tenetur molestiae commodi, voluptas cum mollitia! Totam illo nesciunt laboriosam, cumque libero quibusdam provident voluptas maxime nisi blanditiis magni voluptate et ipsa officiis explicabo deserunt reiciendis! </p>",
-        ]);
-
-        Post::create([
-            "user_id" => $user->id,
-            "category_id" => $work->id,
-            "title" => "My work Post",
-            "slug" => "my-second-post",
-            "excerpt" =>
-                "<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit accusamus explicabo adipisci pariatur provident! In atque magni consectetur, perferendis consequatur et id aliquam, sit, quos delectus libero minus repellendus alias! </p>",
-            "body" => "<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium dicta quo atque, ipsum soluta pariatur culpa repellat nostrum quaerat iusto nisi similique odio numquam maiores tempora quas officia sit provident.
-            Necessitatibus error cumque vel tenetur molestiae commodi, voluptas cum mollitia! Totam illo nesciunt laboriosam, cumque libero quibusdam provident voluptas maxime nisi blanditiis magni voluptate et ipsa officiis explicabo deserunt reiciendis! </p>",
-        ]);
-
-        Post::create([
-            "user_id" => $user->id,
-            "category_id" => $personal->id,
-            "title" => "My Pesonal Post",
-            "slug" => "my-third-post",
-            "excerpt" =>
-                "<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit accusamus explicabo adipisci pariatur provident! In atque magni consectetur, perferendis consequatur et id aliquam, sit, quos delectus libero minus repellendus alias! </p>",
-            "body" => "<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium dicta quo atque, ipsum soluta pariatur culpa repellat nostrum quaerat iusto nisi similique odio numquam maiores tempora quas officia sit provident.
-            Necessitatibus error cumque vel tenetur molestiae commodi, voluptas cum mollitia! Totam illo nesciunt laboriosam, cumque libero quibusdam provident voluptas maxime nisi blanditiis magni voluptate et ipsa officiis explicabo deserunt reiciendis! </p>",
         ]);
     }
 }
