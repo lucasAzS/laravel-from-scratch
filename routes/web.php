e99b62015faf74aa78629b610a5ed42179978141
Lucas de Azambuja <lucasazsantos@gmail.com>
2021-07-04 22:13:46 -0300

    js dropdown

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::get("/", function () {
     return view("posts", [
         // "posts" => $posts,
         "posts" => Post::latest()->get(),
+        "categories" => Category::all()
     ]);
 });
 
@@ -33,9 +34,17 @@ Route::get("posts/{post:slug}", function (Post $post) {
 });
 
 Route::get("categories/{category:slug}", function (Category $category) {
-    return view("posts", ["posts" => $category->posts]);
+    return view("posts", [
+        "posts" => $category->posts,
+        "currentCategory" => $category,
+        "categories" => Category::all()
+
+    ]);
 });
 
 Route::get("authors/{author:username}", function (User $author) {
-    return view("posts", ["posts" => $author->posts]);
+    return view("posts", [
+        "posts" => $author->posts,
+        "categories" => Category::all()
+    ]);
 });
